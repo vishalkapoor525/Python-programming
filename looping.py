9066e7c2f156e3cac6737453dc2a3574d6b7e3ac
vishalkapoor525 <vishalkapoor525@gmail.com>
2022-11-26 10:12:18 +0000

    odd and even function

--- looping.py
+++ looping.py
@@ -1,1 +1,13 @@
-print("looping concept")
+#Program for finding odd and even number from list of numbers
+numbers = [20,30,40,50,60,7,5,3,6]
+print("created list is : ", numbers)
+
+choice = int(input("Menu item \n 1. print even numbers \n 2. Print odd numbers \n"))
+if choice == 1:
+    for i in numbers:
+        if (i%2==0):
+            print(i)
+else:
+    for i in numbers:
+        if (i%2!=0):
+            print(i)
